c6c234b3e1996769622ecf301eddf4e6f42f4c39
ShivSPGupta <98518769+ShivSPGupta@users.noreply.github.com>
2024-01-16 23:46:41 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import './App.scss';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './containers/home';
 import About from './containers/about';
 import Contact from './containers/contact';
@@ -11,21 +11,35 @@ import Particles from 'react-tsparticles';
 import { loadFull } from 'tsparticles';
 import particles from './utils/particles';
 
+
 function App() {
+
+  const location = useLocation();
+  console.log(location);
 
   const handleInit = async (main)=>{
     await loadFull(main)
   }
+
+  const renderParticleJsInHomePage = location.pathname === "/";
+
   return (
     <div className="App">
     {/* particles js */}
-    <Particles id='particles' options={particles} init={handleInit}/>
+
+    {
+      renderParticleJsInHomePage && 
+      <Particles id='particles' options={particles} init={handleInit}/>
+    }
+    
 
     {/* navbar */}
     <Navbar/>
 
     {/* main page content */}
-      <Routes>
+
+    <div className='App__main-page-content'>
+    <Routes>
       <Route index path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/contact' element={<Contact/>}/>
@@ -33,6 +47,8 @@ function App() {
       <Route path='/resume' element={<Resume/>}/>
       <Route path='/skills' element={<Skills/>}/>
       </Routes>
+    </div>
+
     </div>
   );
 }
--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import './styles.scss';
 
 const Home=()=>{
     return(
-        <div>
-            Home Page
+        <section id='home' className='home'>
+        <div className='home__text-wrapper'>
+            <h1>
+                Hello, I'm Shiv Shankar
+                <br/>
+                Front end developer
+            </h1>
         </div>
+
+        </section>
     )
 }
 
