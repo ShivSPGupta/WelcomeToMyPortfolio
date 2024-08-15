38316761fc6ce244408b1dd28cd4c22512a7e1c7
ShivSPGupta <98518769+ShivSPGupta@users.noreply.github.com>
2024-08-15 21:16:04 +0530

    first commit

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PageHeaderContent from '../../components/PageHeaderContent';
-import {BsInfoCircleFill} from 'react-icons/bs';
+import { BsInfoCircleFill } from 'react-icons/bs';
 import { Animate } from 'react-simple-animate';
 import './styles.scss';
+import { DiApple, DiAndroid } from 'react-icons/di'
+import { FaDev, FaDatabase } from 'react-icons/fa'
 
 
 const personalDetails = [
@@ -31,52 +33,68 @@ const personalDetails = [
 const jobSummary = 'Seasoned and independent Front End Developer, very passionate about aesthetics and UI design. It is imperative that you provide a thorough and professional approach to your resume. As a Front End Developer you will be judged by your ability to use UX and UI concepts and follow design guidelines. It is about expressing your attention to detail and how you can help implement design ideas for your future employer. '
 
 
-const About=()=>{
-    return(
+const About = () => {
+    return (
         <section id='about' className='about'>
-            <PageHeaderContent 
-            headerText = 'About Me' 
-            icon ={<BsInfoCircleFill size={40}/>}/>
+            <PageHeaderContent
+                headerText='About Me'
+                icon={<BsInfoCircleFill size={40} />} />
 
-            
+
             <div className='about__content'>
-            <div className='about__content__personalWrapper'>
+                <div className='about__content__personalWrapper'>
 
-            <Animate play duration={1.5} delay={1} 
-        start={{
-            transform : 'translateX(-900px)'
-        }}
-        end={{
-            transform : 'translateX(0px)'
-        }}>
-            <h3>Front End Developer</h3>
-            <p>{jobSummary}</p>
-            </Animate>
+                    <Animate play duration={1.5} delay={1}
+                        start={{
+                            transform: 'translateX(-900px)'
+                        }}
+                        end={{
+                            transform: 'translateX(0px)'
+                        }}>
+                        <h3>Front End Developer</h3>
+                        <p>{jobSummary}</p>
+                    </Animate>
 
 
-            <Animate play duration={1.5} delay={1} 
-        start={{
-            transform : 'translateX(500px)'
-        }}
-        end={{
-            transform : 'translateX(0px)'
-        }}>
-            <h3 className='personalInformationHeaderText'>Personal Information</h3>
-            <ul>
-                {
-                    personalDetails.map((item,i)=>(
-                        <li key={i}>
-                            <span className='title'>{item.label}</span>
-                            <span className='value'>{item.value}</span>
-                        </li>
-                    ))
-                }
-            </ul>
-            </Animate>
+                    <Animate play duration={1.5} delay={1}
+                        start={{
+                            transform: 'translateX(500px)'
+                        }}
+                        end={{
+                            transform: 'translateX(0px)'
+                        }}>
+                        <h3 className='personalInformationHeaderText'>Personal Information</h3>
+                        <ul>
+                            {
+                                personalDetails.map((item, i) => (
+                                    <li key={i}>
+                                        <span className='title'>{item.label}</span>
+                                        <span className='value'>{item.value}</span>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </Animate>
+                </div>
+                <div className='about__content__servicesWrapper'>
+                <Animate play duration={1.5} delay={1}
+                        start={{
+                            transform: 'translateX(500px)'
+                        }}
+                        end={{
+                            transform: 'translateX(0px)'
+                        }}>
+                    <div className='about__content__servicesWrapper__innerContent'>
+                        <div>
+                            <FaDev size={60} color='var( --yellow-theme-main-color)' />
+                        </div>
+                        <div><DiAndroid size={60} color='var( --yellow-theme-main-color)' /></div>
+                        <div><FaDatabase size={60} color='var( --yellow-theme-main-color)' /></div>
+                        <div><DiApple size={60} color='var( --yellow-theme-main-color)' /></div>
+                    </div>
+                    </Animate>
 
-            </div>
-            <div className='about__content__servicesWrapper'>Services Wrapper</div>
-
+                </div>
 
             </div>
 
