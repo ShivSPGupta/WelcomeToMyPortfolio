8b0562e764df526744d1b70d90292e9f63aec538
ShivSPGupta <sspgupta1@yahoo.com>
2025-05-12 01:19:21 +0530

    first commit

---
+++ src/containers/skills/SkillsData.jsx
@@ -0,0 +1,28 @@
+export const SkillsData = [
+    {
+        label: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: "90",
+            },
+            {
+                skillName: "CSS",
+                percentage: "50",
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage: "80",
+            },
+        ],
+    },
+    {
+        label: "BACK END",
+        data: [
+            {
+                skillName: "NODE",
+                percentage: "60",
+            },
+        ],
+    },
+]
--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import PageHeaderContent from '../../components/PageHeaderContent';
-import { BsInfoCircleFill } from 'react-icons/bs';
+import React from "react";
+import PageHeaderContent from "../../components/PageHeaderContent";
+import { BsInfoCircleFill } from "react-icons/bs";
 
-const Skills=()=>{
-    return(
-        <section id='skills' className='skills'>
-            <PageHeaderContent 
-            headerText = 'My Skills' 
-            icon ={<BsInfoCircleFill size={40}/>}/>
-        </section>
-    )
-}
+const Skills = () => {
+  return (
+    <section id="skills" className="skills">
+      <PageHeaderContent
+        headerText="My Skills"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="skills__content-wrapper">
+        {
+            
+        }
+      </div>
+    </section>
+  );
+};
 
 export default Skills;
